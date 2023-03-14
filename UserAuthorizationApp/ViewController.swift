423fe012316814315a97841998a32166784ea588
Dmitry <borodulkin_d@mail.ru>
2023-03-14 21:37:03 +0300

    Initial Commit

---
+++ UserAuthorizationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserAuthorizationApp
+//
+//  Created by Дмитрий Бородулькин on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
