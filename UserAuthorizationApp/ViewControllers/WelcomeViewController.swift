75adc164812f217dec8bc9569c0829127d9664db
Dmitry <borodulkin_d@mail.ru>
2023-03-22 23:04:01 +0300

    Redesigned the logic of the work of the WelcomeViewController.

--- UserAuthorizationApp/ViewControllers/WelcomeViewController.swift
+++ UserAuthorizationApp/ViewControllers/WelcomeViewController.swift
@@ -11,16 +11,25 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
     
-    var user = ""
+    var user: User!
     
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeLabel.text = "Welcome, \(user)!"
-   
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        welcomeLabel.text = "Welcome, \(user.person.fullName)!"
     }
-    
-
-
-
 }
+
